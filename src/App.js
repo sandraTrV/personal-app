4287c31c4353f9b1a4d7e244fdea5990d1a34443
SandraVatr <sandravatr@gmail.com>
2023-11-23 13:58:27 +0200

    images added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import moon from './moon.png';
+import sun from './sun.png';
 function App() {
   return (
     <div className="App">
+    
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h2>AAAAAAAa</h2>
+        <img src={moon} className="App-logo" alt="logo" />
+        <img src={sun} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and sa zxcsxcsADve to reload.
         </p>
