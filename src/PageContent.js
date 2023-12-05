c3f2427388e5d7cd3e247d484ab85a9539c5998b
SandraVatr <sandravatr@gmail.com>
2023-12-05 17:19:01 +0200

    change images at every project, project description

--- src/PageContent.js
+++ src/PageContent.js
@@ -10,14 +10,16 @@ import Button from '@mui/material/Button';
 import sun from './front.jpg';
 import {useState} from 'react';
 import { dialogActionsClasses } from '@mui/material';
+import Grow from '@mui/material/Grow';
 // import {useEffect} from 'react';
 function PageContent() {
   const [projectIsShown, setProjectIsShown] = useState(true);
   const [project, setProject] = useState(0);
   const appsList = [
-    {
+    { 
+      projectNo: 0,
       projectName : 'Fridge Notes App',
-      projectDescription : 'Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 ',
+      projectDescription : 'A user-friendly, colorful and nostalgic React App that looks like a vintage fridge. It uses your browser\'s memory to keep your to-do lists, recipes or any small notes that you would put on your fridge. With a few clicks you can create, delete or edit your notes. Moreover, this fridge design looks good on a multitude of devices.',
       projectToolsList : ['JS', 'HTML+CSS', 'React.js', 'MUI'],
       projectLinkList : {
         websiteLnk : 'https://www.pngwing.com/en/free-png-bcahmm',
@@ -25,9 +27,10 @@ function PageContent() {
       }
       
     },
-    {
+    { 
+      projectNo: 1,
       projectName : 'Quiz App',
-      projectDescription : 'Description 2 Description 2 Description 2 Description 2 Description 2 Description 2 ',
+      projectDescription : 'This is an easy to use and responsive quiz app that helps you test your knowledge in web development. At the end of each quiz you can see your score and a report of your answers and the right ones in case you got some questions wrong.',
       projectToolsList : ['JS', 'HTML+CSS', 'Bootstrap', 'jQuery'],
       projectLinkList :  {
         websiteLnk : 'https://google.com',
@@ -36,8 +39,9 @@ function PageContent() {
       
     },
     {
+      projectNo: 2,
       projectName : 'The CatSite',
-      projectDescription : 'Description 3 Description 3 Description 3 Description 3 Description 3 Description 3 ',
+      projectDescription : 'With the help of an API this online cat guide brings you into the world of cats. Here you can learn a significant amount of facts about over 60 cat breeds using our detailed cat catalogue. If you want more, you can also see a huge amount of random cat photos that will make your day CATtastic! If you have a cat or you are planning to get one, this website is the guide you need!',
       projectToolsList : ['JS', 'HTML+CSS', 'Bootstrap', 'API'],
       projectLinkList :  {
         websiteLnk : 'https://www.pinterest.com/pin/358528820352973949/',
@@ -80,21 +84,29 @@ function PageContent() {
       <div className='description'>
         <Grid container spacing={5} >
       
-          <Grid className='aboutTxt' item xs={12} sm={12} md={7} lg={8}> 
+          <Grid className='aboutTxt' item xs={12} sm={12} md={12} lg={8}> 
             {projectIsShown && (
               <div>
+              {/* <Grow
+                in={true}
+                style={{ transformOrigin: '0 0 0' }}
+                {...(true ? { timeout: 1000 } : {})}
+  > */}
                 <ProjectDescr p={appsList[project]}/>
+                {/* </Grow> */}
               </div>
             ) || (  !projectIsShown  && (
-              <div>
+              <div> 
                 <ProjectsGeneralInfo/>
               </div>
             )
             )
           }
           </Grid>
-          <Grid  item xs={12} sm={12} md={5} lg={4}>
+          <Grid  item xs={12} sm={12} md={12} lg={4}>
+          
           <Projects setCurrentPr={changeCurrentProjectHere}/>
+          
           </Grid>
         </Grid>
        
--- src/ProjectDescr.js
+++ src/ProjectDescr.js
@@ -1,9 +1,15 @@
 import './ProjectDescr.css';
 import im from './front.jpg';
+import sun from './sun.png';
+import moon from './moon.png';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import {useState, useEffect} from 'react';
 
+import {useState, useEffect} from 'react';
+// import MuiImageSlider from 'mui-image-slider';
+import Grow from '@mui/material/Grow';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 function ProjectDescr(props) {
   const openSite = () => {
     console.log(props.p.projectLinkList.websiteLnk);
@@ -15,60 +21,46 @@ function ProjectDescr(props) {
     // const projectDesplayed = JSON.parse(localStorage.getItem('projectDisplayed'));
     // console.log('current project is ' + props.p.projectDescription);
   }
-  // const [currentProject, setCurrentProject] = useState(0);
-  // useEffect(() => {
-  //   // setCurrentProject(Number(props.p));
-  //   // document.getElementById('projectDescription').innerHTML = appsList[currentProject].projectDescription;
-  //   const projectDesplayed = JSON.parse(localStorage.getItem('projectDisplayed'));
-  //   if (projectDesplayed) {
-  //     // console.log('user notes found');
-  //     setCurrentProject(Number(projectDesplayed));
-  //     console.log('hhhhhhhhhhhhhhhhh');
-  //     document.getElementById('projectDescription').innerHTML = appsList[currentProject].projectDescription;
-  //   } 
+
+  const images = [
+    [im, sun, moon], [sun, moon, im], [moon, im, sun]
+  ];
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const moveToNextImg = () => {
+    console.log('next, current is ' + currentImage);
     
-  // }, []);
-  // setCc(props.p);
-  // localStorage.setItem('userNotes', JSON.stringify(newNotes));
-  // const appsList = [
-  //   {
-  //     projectName : 'Fridge Notes App',
-  //     projectDescription : 'Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 ',
-  //     projectToolsList : ['JS', 'HTML+CSS', 'React.js', 'MUI'],
-  //     projectLinkList : [ {
-  //       websiteLnk : 'https://google.com',
-  //       codeLink : 'https://google.com'
-  //     }
-  //     ]
-  //   },
-  //   {
-  //     projectName : 'Quiz App',
-  //     projectDescription : 'Description 3 Description 3 Description 3 Description 3 Description 3 Description 3 ',
-  //     projectToolsList : ['JS', 'HTML+CSS', 'Bootstrap', 'jQuery'],
-  //     projectLinkList : [ {
-  //       websiteLnk : 'https://google.com',
-  //       codeLink : 'https://google.com'
-  //     }
-  //     ]
-  //   },
-  //   {
-  //     projectName : 'The CatSite',
-  //     projectDescription : 'Description 2 Description 2 Description 2 Description 2 Description 2 Description 2 ',
-  //     projectToolsList : ['JS', 'HTML+CSS', 'Bootstrap', 'API'],
-  //     projectLinkList : [ {
-  //       websiteLnk : 'https://google.com',
-  //       codeLink : 'https://google.com'
-  //     }
-  //     ]
-  //   }
-  // ]
-  return (
+    if (currentImage < 2) {
+      console.log('next, current is ' + currentImage);
+      setCurrentImage(currentImage+1);
+      document.getElementById('appImage').src = images[props.p.projectNo][currentImage+1];
+    }
+  }
+
+  const moveToPrevImg = () => {
+    console.log('prev, current is ' + currentImage);
+    if (currentImage >= 1) {
+      console.log('prev, current is ' + currentImage);
+      setCurrentImage(currentImage-1);
+      document.getElementById('appImage').src = images[props.p.projectNo][currentImage-1];
+    }
+  }
+  return ( 
+    
     <div>
     <h2 id='projectName' className='projectTitle'>{props.p.projectName}</h2>
-    <img src={im} className="appImage" alt="logo" /> 
-    
+    {/* <Box className='ce'> */}
+      {/* <KeyboardArrowLeftIcon onClick={moveToPrevImg} id='prev' className='prev' style={{color:'#ffffff'}}></KeyboardArrowLeftIcon> */}
+
+         <img src={images[props.p.projectNo][0]} id='appImage' className="appImage" alt="logo" /> 
+      
+      {/* <KeyboardArrowRightIcon onClick={moveToNextImg} id='next' className='next' style={{color:'#ffffff'}}></KeyboardArrowRightIcon> */}
+    {/* </Box> */}
     <div className='text'>
-      <p id='projectDescription'>{props.p.projectDescription} </p>
+ 
+          <p className='ddd' id='projectDescription'>{props.p.projectDescription} </p>
+        
+       
       <Box  sx={{display: 'flex', flexWrap: 'wrap', padding: '0', marginLeft: '0', textAlign:'left'}}>
         <Button variant='text' className='toolsName'>{props.p.projectToolsList[0]}</Button>  
         <Button variant='text' className='toolsName'>{props.p.projectToolsList[1]}</Button> 
